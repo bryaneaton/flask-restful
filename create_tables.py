90158fea0dd0d30e3af6e59bb2d3ca786c1c50d6
bryaneaton <sfasu77@gmail.com>
2017-11-12 20:29:48 -0500

    initial commit

--- create_tables.py
+++ create_tables.py
@@ -5,9 +5,12 @@ connection = sqlite3.connect('data.db')
 
 cursor = connection.cursor()
 
-create_table = 'create table IF NOT EXISTS users(id INTEGER primary key autoincrement, username text, password text)'
+create_users = 'create table IF NOT EXISTS users(id INTEGER primary key autoincrement, username text, password text)'
+create_items = 'create table if not exists items (id integer primary key AUTOINCREMENT , name text, price real)'
 
-cursor.execute(create_table)
+
+cursor.execute(create_users)
+cursor.execute(create_items)
 
 connection.commit()
 
