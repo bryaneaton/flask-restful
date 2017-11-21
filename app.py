35abd3fe64d8fbcda9043b44a90d498504fc2812
bryaneaton <sfasu77@gmail.com>
2017-11-21 12:09:01 -0500

    Working SQL Alchemy models and resources.  Need to add Store model and resource.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import pyodbc
 import psycopg2
+
+
 from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
@@ -8,16 +10,15 @@ from resources.item import Item, ItemList
 from resources.user import UserRegister
 from resources.store import Store, StoreList
 from security import authenticate, identity
-
+from config import mssqlConfig, postgresqlConfig
 
 # params = urllib.quote_plus("DRIVER={SQL Server Native Client 11.0};SERVER=CTG-SQLLAB01>;DATABASE=CTG_Custom;UID=dsuser;PWD=dsu3r_123;driver=[SQL Server]")
 # SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # SQL lite connection string
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://dsuser:dsu3r_123@CTG-SQLLAB01:1433/ITQ?driver=SQL+Server+Native+Client+10.0" #SQL Server Credentials
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://alexis:Alexis2014@localhost/custom"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = postgresqlConfig
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Dese.Decent.Pups.BOOYO0OST'
 api = Api(app)
